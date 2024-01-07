f1d6996aa37df68fe5773c6c9ef2ba388dd587aa
dina-petrick <dinapetrick@gmail.com>
2024-01-07 17:21:07 +0530

    product card style changed

--- src/components/category-preview/category-preview.style.jsx
+++ src/components/category-preview/category-preview.style.jsx
@@ -14,18 +14,27 @@ export const Title = styled.h2`
   cursor: pointer;
 `
 
+
+
+
 export const TitleLink = styled(Link)`
   font-size: 28px;
-  letter-spacing: 4px;
-  transition : letter-spacing 0.2s ease;
+  letter-spacing: 1px;
+  transition : letter-spacing 0.4s ease;
   &:hover{
-    letter-spacing: 1px
+    letter-spacing: 4px;
   }
 `
 
 export const Arrow = styled.span`
   display:inline-block;
   margin-left: 5px;
+  transition : margin-left 0.2s ease;
+
+  ${TitleLink}:hover & {
+    margin-left: 12px;
+  }
+
 `
 
 export const Preview = styled.div`

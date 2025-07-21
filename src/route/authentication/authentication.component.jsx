65fbbbdfb2c0eb9a79d866c4d9dea64eba200d28
Dinakaran <110745337+Dina-petrick@users.noreply.github.com>
2025-07-21 04:08:57 +0000

    Navigation changes

--- src/route/authentication/authentication.component.jsx
+++ src/route/authentication/authentication.component.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, {useContext, useEffect} from 'react'
 import { AuthenticationContainer } from './authentication.style';
+import { useNavigate } from 'react-router-dom';
 
 import SignUpForm from '../../components/sign-up-form/signUpForm.component';
 import SignInForm from '../../components/sign-in-form/signInForm.component';
 
+import { UserContext } from "../../context/UserProvider.context";
+
 
 const Authentication = () => {
-  return(
+  const navigate = useNavigate();
+  const { currentUser } = useContext(UserContext);
+
+useEffect(() => {
+  if (currentUser) {
+    navigate('/');
+  }
+}, [currentUser, navigate]);
+
+  return (
     <AuthenticationContainer>
       <SignInForm />
       <SignUpForm />
